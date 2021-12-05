67eb3d495de1793b7345b4e5051337ba02648a96
Ivan_Zhyuliuk <zhyuliuk.ivan@gmail.com>
2021-12-06 02:28:57 +0300

    comment tests without auth

--- controller/src/main/java/com/epam/esm/config/ApplicationRunner.java
+++ controller/src/main/java/com/epam/esm/config/ApplicationRunner.java
@@ -8,5 +8,7 @@ public class ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRunner.class, args);
+        System.setProperty("javax.net.ssl.trustStore", "C:/Users/Ivan_Zhyuliuk/keystore.p12");
+        System.setProperty("javax.net.ssl.trustStorePassword", "Qwe123okA123");
     }
 }
