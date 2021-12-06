2c6230af42ff51315aaf2392a96d237a8a9826c9
Ivan_Zhyuliuk <zhyuliuk.ivan@gmail.com>
2021-12-06 18:59:02 +0300

    comment tests without auth

--- controller/src/main/java/com/epam/esm/config/ApplicationRunner.java
+++ controller/src/main/java/com/epam/esm/config/ApplicationRunner.java
@@ -1,10 +1,13 @@
 package com.epam.esm.config;
 
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "com.epam.esm")
 public class ApplicationRunner extends SpringBootServletInitializer {
 
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunner.class, args);
+    }
 }
